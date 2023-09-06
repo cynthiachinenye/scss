b4a93f12a9b07f2c7996cb4f448abb786df3ab40
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-06 16:04:29 +0100

    new

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const {src, dest,watch,series}= require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles(){
-    return src('*.scss')
+    return src('cynthia/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask(){
-    watch(['*.scss'],buildStyles)
+    watch(['cynthia/**/*.scss'],buildStyles)
 }
 
 exports.default = series(buildStyles,watchTask)
